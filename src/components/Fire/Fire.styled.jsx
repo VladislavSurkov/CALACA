e9c1d133381c8b42929d97f50a4603d2210c1e5d
Maksym Lepikhov <Werzeragna@icloud.com>
2023-06-30 14:41:56 +0300

    Fire positioning
    Positioning of fire, size, styles, etc.

--- src/components/Fire/Fire.styled.jsx
+++ src/components/Fire/Fire.styled.jsx
@@ -4,16 +4,50 @@ import fire from "../../img/animation/fire.png";
 const POSITION = {
   center: css`
     position: absolute;
-    top: 100px;
+    top: 560px;
+    left: 855px;
   `,
-  first: css``,
-  second: css``,
-  third: css``,
+  firstLeft: css`
+    position: absolute;
+    top: 555px;
+    left: 525px;
+    transform: rotateY(40deg);
+  `,
+  secondLeft: css`
+    position: absolute;
+    top: 610px;
+    left: 450px;
+    transform: rotateY(50deg);
+  `,
+  firstRight: css`
+    position: absolute;
+    top: 555px;
+    left: 1200px;
+    transform: rotateY(40deg);
+  `,
+  secondRight: css`
+    position: absolute;
+    top: 650px;
+    left: 1250px;
+    transform: rotateY(50deg);
+  `,
+  thirdLeft: css`
+    position: absolute;
+    top: 750px;
+    left: 612px;
+    transform: rotateY(60deg);
+  `,
+  thirdRight: css`
+    position: absolute;
+    top: 760px;
+    left: 1020px;
+    transform: rotateY(30deg);
+  `,
 };
 
 export const FireComp = styled.div`
-  width: 300px;
-  height: 300px;
+  width: 200px;
+  height: 200px;
   background-image: url(${fire});
   background-size: contain;
 
