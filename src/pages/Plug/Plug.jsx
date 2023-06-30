c012be9887fc8455aa670be47e8c517c44fd7902
Maksym Lepikhov <Werzeragna@icloud.com>
2023-06-30 18:59:21 +0300

    Fonts and Title

--- src/pages/Plug/Plug.jsx
+++ src/pages/Plug/Plug.jsx
@@ -1,9 +1,11 @@
 import Fire from "components/Fire/Fire";
-import { PlugContainer } from "./Plug.styled";
+import { PlugContainer, SubTitle, Title } from "./Plug.styled";
 
 const Plug = () => {
   return (
     <PlugContainer>
+    <Title>SHAPE YOUR DESTINY</Title>
+    <SubTitle>COMING SOON</SubTitle>
       <Fire position="center" />
       <Fire position="firstLeft"/>
       <Fire position="secondLeft"/>
