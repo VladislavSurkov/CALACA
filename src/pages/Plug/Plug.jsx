286bee8674cc89ce3e79788133aaa4ebb9a3370a
VladislavSurkov <fvhabjrega@gmail.com>
2023-06-30 19:02:56 +0300

    position fire animations

--- src/pages/Plug/Plug.jsx
+++ src/pages/Plug/Plug.jsx
@@ -4,13 +4,7 @@ import { PlugContainer } from "./Plug.styled";
 const Plug = () => {
   return (
     <PlugContainer>
-      <Fire position="center" />
-      <Fire position="firstLeft"/>
-      <Fire position="secondLeft"/>
-      <Fire position="thirdLeft"/>
-      <Fire position="firstRight"/>
-      <Fire position="secondRight"/>
-      <Fire position="thirdRight"/>
+      <Fire/>
     </PlugContainer>
   );
 };
--- src/pages/Plug/Plug.styled.jsx
+++ src/pages/Plug/Plug.styled.jsx
@@ -3,10 +3,11 @@ import background from "../../img/plug-background.png"
 
 
 export const PlugContainer = styled.div`
-background-image: url(${background});
-background-size: contain;
-background-repeat: no-repeat;
-background-position: center;
-width: 1920px;
-height: 1080px;
-`
+  display: flex;
+  justify-content: center;
+  background-image: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 1080px;
+`;
