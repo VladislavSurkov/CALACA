9765c39683392ee0fe00b96d111e4915bd3cd669
Maksym Lepikhov <Werzeragna@icloud.com>
2023-07-01 14:21:16 +0300

    Pre-fix

--- src/components/Social/Social.styled.js
+++ src/components/Social/Social.styled.js
@@ -22,6 +22,8 @@ export const SocialMediaList = styled.ul`
   display: flex;
   flex-direction: row;
   gap: 30px;
+  justify-content: center;
+  align-items: center;
   border: 1px solid red;
   width: 346px;
   height: 88px;
@@ -29,10 +31,15 @@ export const SocialMediaList = styled.ul`
 
 export const SocialMediaItem = styled.li`
   list-style: none;
+  
 `;
 
 export const SocialMediaLink = styled.a`
+display: flex;
+flex-direction: column;
+
   width: 64px;
   height: 64px;
+
   ${(props) => props.mediaType && MEDIATYPE[props.mediaType]};
 `;
