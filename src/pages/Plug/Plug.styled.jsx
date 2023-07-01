2c2d95d78e867aee4cc7c32439e4c10b135ad030
VladislavSurkov <fvhabjrega@gmail.com>
2023-07-01 15:44:39 +0300

    update Plug

--- src/pages/Plug/Plug.styled.jsx
+++ src/pages/Plug/Plug.styled.jsx
@@ -1,49 +1,88 @@
 import styled from "styled-components";
-import background from "../../img/bg/plug-background.png";
+import backgroundDesktop from "../../img/bg/bg-desktop.png";
+import backgroundMobile from "../../img/bg/bg-mobile.png";
 
 export const PlugContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
   height: 1080px;
 
-  background-image: url(${background});
+  background-image: url(${backgroundMobile});
   background-size: cover;
   background-repeat: no-repeat;
   background-position: center;
+
+  @media (min-width: 468px) {
+    background-image: url(${backgroundDesktop});
+  }
+`;
+
+export const Logo = styled.img`
+  width: 140px;
+  height: 70px;
+
+  margin-top: 42px;
+
+  @media (min-width: 468px) {
+    width: 190px;
+    height: 90px;
+    margin-top: 50px;
+  }
+
+  @media (min-width: 920px) {
+    width: 210px;
+    height: 100px;
+  }
 `;
 
 export const Title = styled.h1`
-  display: flex;
-
   font-family: "ST-SimpleSquare";
-  font-weight: 500;
-  font-size: 110px;
+  font-weight: 400;
+  font-size: 64px;
   line-height: 0.9;
-  text-align: center;
-  overflow-wrap: break-word;
 
+  text-align: center;
   max-width: 1200px;
-  margin: 0 0 40px 0;
+  margin-top: 48px;
 
   color: #eee3ff;
+
+  @media (min-width: 468px) {
+    font-weight: 400;
+    font-size: 80px;
+    line-height: 0.9;
+    margin-top: 50px;
+  }
+
+  @media (min-width: 920px) {
+    font-weight: 400;
+    font-size: 118px;
+    line-height: 0.9;
+  }
 `;
 
 export const SubTitle = styled.h2`
   font-family: "Roboto Condensed";
-  color: #eee3ff;
   font-weight: 700;
-  letter-spacing: 28px;
-  -webkit-text-stroke: 1px black;
+  letter-spacing: 13px;
 
-  font-size: 36px;
+  font-size: 16px;
   margin: 0;
   padding-left: 28px;
-`;
+  margin-top: 40px;
 
-export const Logo = styled.img`
-width: 210px;
-height: 90px;
-margin-bottom: 50px;
-margin-top: 60px;
-`
+  color: #eee3ff;
+  @media (min-width: 468px) {
+    font-weight: 500;
+    letter-spacing: 20px;
+    font-size: 20px;
+    margin-top: 20px;
+  }
+
+  @media (min-width: 920px) {
+    letter-spacing: 30px;
+    font-size: 36px;
+  }
+`;
