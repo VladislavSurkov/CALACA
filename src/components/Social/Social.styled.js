808bda893411ad3f877ad0692f06e56627a7fd46
Maksym Lepikhov <Werzeragna@icloud.com>
2023-07-01 17:41:48 +0300

    Fire update

--- src/components/Social/Social.styled.js
+++ src/components/Social/Social.styled.js
@@ -1,35 +1,43 @@
 import styled from "styled-components";
 
-
 export const SocialMediaList = styled.ul`
   display: flex;
   flex-direction: row;
   gap: 30px;
   justify-content: center;
   align-items: center;
-  /* border: 1px solid red; */
   width: 346px;
   height: 88px;
+  position: absolute;
+  bottom: 28px;
+
 `;
 
 export const SocialMediaItem = styled.li`
   list-style: none;
   text-align: center;
+  @media screen and (min-width: 468px) {
+    &:nth-child(2) {
+      margin-right: 50px;
+    }
+  }
+  transition:all 400ms ease-in-out;
 
-  
+  &:hover {
+    transform: scale(1.1);
+    text-shadow: 1px 1px 1px black;
+  }
 `;
 
 export const SocialMediaLink = styled.a`
-display: flex;
-flex-direction: column;
+  display: flex;
+  flex-direction: column;
 
-font-family: "Roboto Condensed";
-font-size: 14px;
-font-weight: 400;
-color: #ffffff50;
-
+  font-family: "Roboto Condensed";
+  font-size: 14px;
+  font-weight: 400;
+  color: #ffffff50;
 
   width: 64px;
   height: 64px;
-
 `;
